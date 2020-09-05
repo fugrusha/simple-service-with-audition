a5df2871a5a1d65d8ae42908d32b7763783b8397
holovko.a <andrey.holovko@nixsolutions.com>
2020-09-05 11:12:09 +0300

    added inserting data from json to registry table

---
+++ src/test/java/com/smida/registry/repository/InsertDataLiquibaseTest.java
@@ -0,0 +1,28 @@
+package com.smida.registry.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@Sql(statements = {"delete from registry"},
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@TestPropertySource(properties = "spring.liquibase.change-log=classpath:db/changelog/db.changelog-master.xml")
+public class InsertDataLiquibaseTest {
+
+    @Autowired
+    private RegistryRepository registryRepository;
+
+    @Test
+    public void testInsertData() {
+        Assert.assertTrue(registryRepository.count() > 0);
+    }
+}
